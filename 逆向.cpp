599624f864ebc6c6da1961151fa93b24b81b4623
lanhuahuahua <91480213+lanhuahuahua@users.noreply.github.com>
2021-10-19 20:18:15 +0800

    Add files via upload

--- 逆向.cpp
+++ 逆向.cpp
@@ -1,11 +1,23 @@
 #include<stdio.h>
 int main(){
 	int n,a,b[20],c,d[20],x=0;
-	scanf("%d\n",&n);
+	while(1)
+	{
+	scanf("%d",&n);
+	if(n>0&&n<=20)      
+	{
 	for(a=0;a<n;a++)
 	scanf("%d",&b[a]);
 	for(a=n-1;a>=0;a--)
-	printf("%d ",b[a]);
+	printf("%d ",b[a]);}
+    else if(n<0||n>20)
+    printf("input Error");
+    else if(n==0)
+    {
+	printf("End Of input");
+    break;
+	}
+    }	
 	return 0;
 	}
 	
