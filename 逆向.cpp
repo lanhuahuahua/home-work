5254e59f0c653c04ac20cc8dc78c80f63b7a3823
lanhuahuahua <91480213+lanhuahuahua@users.noreply.github.com>
2021-10-19 19:46:33 +0800

    Add files via upload

---
+++ 逆向.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int n,a,b[20],c,d[20],x=0;
+	scanf("%d\n",&n);
+	for(a=0;a<n;a++)
+	scanf("%d",&b[a]);
+	for(a=n-1;a>=0;a--)
+	printf("%d ",b[a]);
+	return 0;
+	}
+	
+	
+    
+	
+	
+ 
